c4fd23d7bcbbb6c41a2f2a03bfd13f40a6b1c6d0
ellamarie005 <ellatolentino05@gmail.com>
2018-10-03 23:28:56 -0700

    changed styling

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import IncomeEntries from './components/IncomeEntries';
 import ExpenseEntries from './components/ExpenseEntries';
 import Summary from './components/Summary';
+import styled from 'styled-components';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div className='container'>
+      <WeatherApp className='container'>
         <div className='jumbotron' >
-        <div className='hello'>Hello World</div>
           <h1 className='display-3 text-center'>Budget Tracker</h1>
         </div>
         <div className='row'>
@@ -24,7 +24,19 @@ export default class App extends React.Component {
             <Summary />
           </div>
         </div>
-      </div>
+      </WeatherApp>
     );
   }
 }
+
+const WeatherApp = styled.div`
+padding: 50px;
+
+.jumbotron {
+  opacity: 0.7;
+}
+
+.row {
+  opacity: 0.9;
+}
+`
